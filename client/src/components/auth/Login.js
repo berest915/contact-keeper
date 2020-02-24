f4eb7baf223e3af972f2461f21a7d199f9d2adca
elude21933 <43527518+rest-media@users.noreply.github.com>
2020-02-24 17:10:37 +0800

    test

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -53,7 +53,7 @@ const Login = (props) => {
   return (
     <div className="form-container">
       <h1>
-        Account <span className="text-primary">d</span>
+        Account <span className="text-primary">Logsd</span>
       </h1>
       <form onSubmit={onSubmit}>
         <div className="form-group">
